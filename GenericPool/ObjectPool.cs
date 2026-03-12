cb4e3a06f7d731d59b993d0148a973b3383f65d2
JihooKim <82192985+gamjaHoo@users.noreply.github.com>
2026-03-12 21:06:18 +0900

    과제 47 완료

--- GenericPool/ObjectPool.cs
+++ GenericPool/ObjectPool.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace GenericPool
 {
@@ -13,13 +12,50 @@ namespace GenericPool
         public ObjectPool(int maxSize)
         {
             this.maxSize = maxSize;
+            _available = new List<T>();
+            _active = new List<T>();
         }
+
         public T Get()
         {
-            for(int i = 0; i < _available.Count; i++)
-            {
+            T item;
 
-            } 
+            if (_available.Count > 0)
+            {
+                item = _available[0];
+                _available.RemoveAt(0);
+            }
+            else
+            {
+                if (_active.Count + _available.Count >= maxSize)
+                {
+                    return null;
+                }
+
+                item = new T();
+            }
+
+            _active.Add(item);
+            return item;
+        }
+
+        public void Return(T item)
+        {
+            if (_active.Remove(item))
+            {
+                item.Reset();
+                _available.Add(item);
+            }
+        }
+
+        public int ActiveCount
+        {
+            get { return _active.Count; }
+        }
+
+        public int AvailableCount
+        {
+            get { return _available.Count; }
         }
     }
 }
--- GenericPool/Program.cs
+++ GenericPool/Program.cs
@@ -6,7 +6,40 @@ namespace GenericPool
     {
         static void Main(string[] args)
         {
+            ObjectPool<Bullet> pool = new ObjectPool<Bullet>(3);
 
+            Console.WriteLine("=== 총알 발사 ===");
+
+            Bullet b1 = pool.Get();
+            b1.Fire(10, 20);
+
+            Bullet b2 = pool.Get();
+            b2.Fire(30, 40);
+
+            Bullet b3 = pool.Get();
+            b3.Fire(50, 60);
+
+            Console.WriteLine($"활성: {pool.ActiveCount}, 비활성: {pool.AvailableCount}");
+
+            Console.WriteLine("\n=== 풀 초과 시도 ===");
+
+            Bullet b4 = pool.Get();
+            if (b4 == null)
+            {
+                Console.WriteLine("풀이 가득 찼습니다!");
+            }
+
+            Console.WriteLine("\n=== 반납 후 재사용 ===");
+
+            pool.Return(b1);
+            Console.WriteLine("총알 반납됨");
+
+            Console.WriteLine($"활성: {pool.ActiveCount}, 비활성: {pool.AvailableCount}");
+
+            Bullet b5 = pool.Get();
+            b5.Fire(100, 200);
+
+            Console.WriteLine($"활성: {pool.ActiveCount}, 비활성: {pool.AvailableCount}");
         }
     }
 }
